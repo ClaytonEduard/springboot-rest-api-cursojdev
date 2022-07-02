e186fbf56e797babff9fbfcfb334656a4b767c8d
ClaytonEduard <clayton_eduard@hotmail.com>
2022-07-02 15:27:58 -0300

    :sparkles: Implementando o repository buscar no por nome

--- springboot-rest-api/src/main/java/br/com/springboot/cursojdev/repository/UsuarioRepository.java
+++ springboot-rest-api/src/main/java/br/com/springboot/cursojdev/repository/UsuarioRepository.java
@@ -1,11 +1,18 @@
 package br.com.springboot.cursojdev.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.springboot.cursojdev.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
+	
+	@Query(value = "select u from Usuario u where u.nome like %?1%")// ?1 somente para busca de um dado, no caso o nome 
+	List<Usuario> buscarPorNome(String name);
+	
+	
 }
