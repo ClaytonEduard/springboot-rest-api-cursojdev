aa00b01fa08c965f480e8181bd2649eed57c493e
ClaytonEduard <clayton_eduard@hotmail.com>
2022-07-02 15:42:21 -0300

    :wrench: melhorando o metodo de pesquisa por nome

--- springboot-rest-api/src/main/java/br/com/springboot/cursojdev/repository/UsuarioRepository.java
+++ springboot-rest-api/src/main/java/br/com/springboot/cursojdev/repository/UsuarioRepository.java
@@ -9,10 +9,12 @@ import org.springframework.stereotype.Repository;
 import br.com.springboot.cursojdev.model.Usuario;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-	
-	@Query(value = "select u from Usuario u where u.nome like %?1%")// ?1 somente para busca de um dado, no caso o nome 
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	@Query(value = "select u from Usuario u where upper(trim(u.nome)) like %?1%") // ?1 somente para busca de um dado, no caso
+																			// o nome / trim para retirar os espaços do
+																			// vindo co banco
+	// upper para tornar maiusculas
 	List<Usuario> buscarPorNome(String name);
-	
-	
+
 }
